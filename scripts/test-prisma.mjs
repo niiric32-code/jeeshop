dc11c50923fabec2425170d18f2f585403f7fa40
niiric32-code <niiric32-code@users.noreply.github.com>
2025-12-08 07:31:22 -0800

    chore: remove temporary test files

--- scripts/test-prisma.mjs
+++
@@ -1,9 +0,0 @@
-import('../lib/prisma.js')
-  .then(m => {
-    console.log('imported prisma');
-    console.log('has $connect:', typeof m.default.$connect);
-  })
-  .catch(e => {
-    console.error('import error:', e && e.message || e);
-    process.exit(1);
-  });
